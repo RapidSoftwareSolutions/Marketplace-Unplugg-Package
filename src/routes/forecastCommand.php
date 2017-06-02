472737c18e215118b344e5a653be454e93f5a097
ivz-dev <zakutynsky@yahoo.com>
2017-06-02 16:13:53 +0300

    test webhook

--- src/routes/forecastCommand.php
+++ src/routes/forecastCommand.php
@@ -1,5 +1,5 @@
 <?php
 
 $app->post('/api/Unplugg/forecastCommand', function ($request, $response) {
-    return $response->withStatus(200)->write($request->getBody());
+    echo $request->getBody();
 });
