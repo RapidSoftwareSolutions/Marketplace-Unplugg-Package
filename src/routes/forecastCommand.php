9b91503d864dc80cbc6968287dafda4b76a2c798
ivz-dev <zakutynsky@yahoo.com>
2017-06-02 14:32:27 +0300

    test webhook

--- src/routes/forecastCommand.php
+++ src/routes/forecastCommand.php
@@ -1,9 +1,10 @@
 <?php
 
 $app->post('/api/Unplugg/forecastCommand', function ($request, $response) {
+
     $result['callback'] = 'success';
     $result['contextWrites']['to']['http_resp'] = "";
-    $result['contextWrites']['to']['client_msg'] = array('message' => 'Hello World');
+    $result['contextWrites']['to']['client_msg'] = $request->getParsedBody();
     $result['contextWrites']['to']['params'] = ["param1"=>"value1", "param2"=>"value2"];
     return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($result);
 
