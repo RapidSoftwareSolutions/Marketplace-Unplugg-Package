146aa029b23a232eb27ab64c9f7c0f0e7b6c8d2d
ivz-dev <zakutynsky@yahoo.com>
2017-06-08 18:53:19 +0300

    test webhook

--- src/routes/forecastCommand.php
+++ src/routes/forecastCommand.php
@@ -1,10 +1,20 @@
 <?php
-
 $app->post('/api/Unplugg/forecastCommand', function ($request, $response) {
-    $body = $request->getBody();
+
+    $data = $request->getBody();
+
+    function normalizeJson($jsonObject)
+    {
+        return preg_replace_callback('~"([\[{].*?[}\]])"~s', function ($match) {
+            return preg_replace('~\s*"\s*~', "\"", $match[1]);
+        }, $jsonObject);
+    }
+
+    $data = json_decode(normalizeJson($data));
+
     $result['contextWrites']['to']['http_resp'] = "ok";
-    $result['contextWrites']['to']['client_msg'] = $body['body'];
-    $result['contextWrites']['to']['params']['token'] = $body['params']['token'];
+    $result['contextWrites']['to']['client_msg'] = $data->body;
+    $result['contextWrites']['to']['params']['token'] = $data->params->token;
     $result['contextWrites']['to'] = json_encode($result['contextWrites']['to']);
     return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($result);
 });
